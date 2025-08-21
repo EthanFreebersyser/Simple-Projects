9e99bf133ab9a0f52a9716fcba3973f2e4196125
Ethan Freebersyser <efreebersyser@optomec.com>
2025-08-21 14:24:40 -0500

    Working version but slow.

--- lv-able/lv-able.cpp
+++ lv-able/lv-able.cpp
@@ -10,36 +10,37 @@ using namespace std;
  */
 
 int main(){
-	int numOfChars;
-	cin >> numOfChars;
-	
-	string s;
-	cin >> s;
+        int numOfChars;
+        cin >> numOfChars;
+        
+        string s;
+        cin >> s;
+        
+        vector<int> lloc(numOfChars);
+        fill(lloc.begin(), lloc.end(), -1);
+        vector<int> vloc(numOfChars);
+        fill(vloc.begin(), vloc.end(), -1);
+        
+        for  (int i = 0; i < numOfChars; i ++){
+                if (s[i] == 'l') lloc.push_back(i);
+                if (s[i] == 'v') vloc.push_back(i);
+        }
+        
+        int numOfSteps= 3;
+        int tempSteps = 3;
+        
+        for (int l: lloc){
+                for (int v: vloc){
 
-	vector<int> lloc(numOfChars);
-	fill(lloc.begin(), lloc.end(), -1);
-	vector<int> vloc(numOfChars);
-	fill(vloc.begin(), vloc.end(), -1);
-
-	for  (int i = 0; i < numOfChars; i ++){
-		if (s[i] == 'l') lloc.push_back(i);
-		if (s[i] == 'v') vloc.push_back(i);
-	}
-
-	int numOfSteps= 3;
-	int tempSteps = 3;
-	for (int l: lloc){
-		for (int v: vloc){
-			if (l + v == 0) tempSteps = 2;
-			
-			if (l == -1 || v == -1) tempSteps = 1;
-			if (v < l || l + 1 < v) tempSteps = 1;
-
-			if (l + 1 == v) tempSteps = 0;
-			
-
-			if(tempSteps < numOfSteps) numOfSteps = tempSteps;
-		}
-	}
-	cout << numOfSteps;
+                        if (l + v == -2) tempSteps = 2;
+                        
+                        if ((l == -1 && v > -1) || (l > -1 && v == -1)) tempSteps = 1;
+                        if (v < l || l + 1 < v) tempSteps = 1;
+                        
+                        if (v - l == 1) tempSteps = 0;
+                        
+                        if(tempSteps < numOfSteps) numOfSteps = tempSteps;
+                }
+        }
+        cout << numOfSteps;
 }
