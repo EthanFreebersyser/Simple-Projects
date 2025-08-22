3c3318b7cf9b62e9c2daa7f2c5ec65ed1e63206c
Ethan Freebersyser <efreebersyser@optomec.com>
2025-08-22 08:07:55 -0500

    First version of lv-able fast, a much simpler solution to the lv-able problem.

---
+++ lv-able/lv-able-fast.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* Author: Ethan Freebersyser
+ * Date: 8/20/2025
+ * This is a improved solution to the kattis problem "lv-able", https://open.kattis.com/problems/lvable
+ */
+
+int main(){
+	int numOfChars;
+	cin >> numOfChars;
+
+	int numOfsteps;
+	if (numOfChars == 0) {
+		cout << 2;
+		return 0;
+	}
+	
+	string s;
+	cin >> s;
+
+	if (s.find("lv") != string::npos) {
+		cout << 0;
+		return 0;
+	}
+
+	bool l = false, v = false;
+	for (char c: s){
+		if (c == 'l') l = true;
+		if (c == 'v') v = true;
+	}
+
+	cout << ((l || v) ? 1 : 2);
+	return 0;
+}
