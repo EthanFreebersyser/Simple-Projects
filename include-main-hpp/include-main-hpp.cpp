9259fab881992d6171a6608d2f13a65c462462b2
Ethan Freebersyser <efreebersyser@optomec.com>
2025-08-22 12:49:07 -0500

    Added abilty to handle multiple inclueds, it is very slow ans exceeds the time limit.

--- include-main-hpp/include-main-hpp.cpp
+++ include-main-hpp/include-main-hpp.cpp
@@ -1,35 +1,48 @@
+
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 /* Author: Ethan Freebersyser
- * Date: 8/20/2025
+ * Date: 8/22/2025
  * This is a solution to the kattis problem "#include main.hpp", https://open.kattis.com/problems/includefile
  */
 
 int main(){
-	int numOfLines;
-	cin >> numOfLines;
-
-	int rLimit;
-	cin >> rLimit;
-
-	int numOfErrors;
-	cin >> numOfErrors;
-
-	string mainS[numOfLines];
-	for (int i = 0; i < numOfLines; i++){
-		cin >> mainS[i];
-	}
-	
-	int32_t errorLoc[numOfErrors];
-        for (int i = 0; i < numOfErrors; i++){
-                cin >> errorLoc[i];
+        int numOfLines, rLimit; long long numOfErrors;
+        cin >> numOfLines >> rLimit >> numOfErrors;
+        
+        string dead;
+        getline (cin, dead);
+        
+        vector <string> lines;
+        vector <string> finalLines; 
+        lines.reserve(numOfLines);
+        for (int i = 0; i < numOfLines; i++) {
+                string temp1;
+                string temp2;
+                getline(cin, temp1);
+                temp2 = temp1;
+                lines.push_back(move(temp1));
+                finalLines.push_back(move(temp2));
         }
-
-        for (int i = 0; i < numOfErrors; i++){
-                cout << mainS[errorLoc[i] % numOfLines];
-        }
-	
-	return 0;
+        
+        vector <long long> errorLoc(numOfErrors);
+        for (int i = 0; i < numOfErrors; i++) cin >> errorLoc[i];
+        
+        for (int i = 0; i < rLimit; i++){
+            int size =  finalLines.size();
+            for (int j = 0; j < size; j++){
+                if (finalLines.at(j) == "#include \"main.hpp\""){
+                    for (int k = numOfLines - 1; k > 0 ; k--){
+                        finalLines.insert(finalLines.begin() + j + 1, lines.at(k));
+                    }
+                }
+            }
+        }   
+        
+        for (int loc: errorLoc) cout << finalLines.at(loc - 1) << "\n";
+        
+        return 0;
 }
